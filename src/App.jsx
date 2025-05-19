9ad5f9ab66d895e7370de7181204e8ecc6fd6c62
Shakib Chakladar <shakibchakladar@gmail.com>
2025-05-19 23:54:45 +0600

    taskboard added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
+import Hero from './components/Hero'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
     <div>
       <Header/>
+      <Hero/>
+      <Footer/>
     </div>
   )
 }
